bda8e259169ee2f53de8c621af096f0845b34fc6
User29422 <100575482+User29422@users.noreply.github.com>
2022-04-28 19:32:52 +0700

    Add files via upload

---
+++ HomeWorkApp/src/HomeWorkApp.java
@@ -0,0 +1,64 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class HomeWorkApp {
+
+    public class FirstClass {
+
+        public static void main(String[] args) {
+
+        }
+    }
+        public static void main(String[] args) {
+            System.out.print("Orange\n" +
+                    "Banana\n" + "Apple");
+        }
+
+    public static void main(String[] args) {
+    }
+
+    public static void main(String[] args) {
+        //numbers
+        int a = 1;
+        int b = 2;
+        System.out.println("a + b");
+        {
+        }
+        System.out.println("a + b = 0 ... 128 = Сумма положительная”) {
+        if {
+            System.out.println("a + b = 0,1 = Сумма отрицательная");
+        }
+    }
+    public static void main(String[] args) {
+        int value = 0;
+         System.out.println("value = 0 = Красный");
+         else {
+             System.out.println("value = 1 ... 100 = Жёлтый ");
+            else {
+                System.out.println("value = 101 ... 2,147,483,647 = Зелёный");
+            }
+        }
+    }
+    public static void main(String[] args) {
+        int a = 1;
+        int b = 2;
+        System.out.println("a > b = a >= b);
+
+        else {
+            System.out.println("a < b = a < b);
+        }
+    }
+    public static void main(String[] args) {
+        int a = 2;
+        int b = 5;
+        System.out.println("a + b = 10...20 = true");
+        else {
+            System.out.println("a + b = 30... = false");
+            public static void fifth(int a){
+                System.out.println(a < 0 ? "-" : "+");
+        }
+
+            public static boolean sixth(int i){
+                return i < 0;
+    }
+ }
